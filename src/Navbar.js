1141da29181b61d00839fb12b7a914bd4c1b529e
sondos-ahmad <201827180@std.sci.cu.edu.eg>
2024-07-29 09:33:34 +0300

    landing page

---
+++ src/Navbar.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static"sx={{ backgroundColor: 'white',height:'70px' }}>
+        <Toolbar>  
+       
+          <img src="/circle-v-red.png" alt="Logo" style={{ height: '80px' }} />
+        
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, color:'black' }}>
+            Viva Culture Center
+          </Typography>
+          <Button sx={{color:'black'}} >CONTACT</Button>
+          <Button sx={{color:'black'}}>ABOUT US</Button>
+          <Button sx={{color:'black'}}>BLOG</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
